cff87a9f75079270cefe9915b468afe8cbccfa5a
jaewon <suv6629@gmail.com>
2023-09-16 16:17:25 +0900

    2133

---
+++ week6/2133.kts
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/2133
+
+fun main() {
+	val n = System.`in`.bufferedReader().readLine().toInt()
+	val dp = IntArray(n + 1)
+	if(n > 1) dp[2] = 3
+	for (i in 4..n step 2) {
+		dp[i] = dp[i - 2] * 3 + 2
+		for (j in 2 until i - 2 step 2) {
+			dp[i] += dp[j] * 2
+		}
+	}
+	println(dp[n])
+}
+
+main()
