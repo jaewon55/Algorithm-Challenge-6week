3850af34bb3772c69f0d04d24cdf0db0f83b6d56
jaewon <suv6629@gmail.com>
2023-09-17 15:45:46 +0900

    2133

--- week6/2133.kts
+++ week6/2133.kts
@@ -2,8 +2,8 @@
 
 fun main() {
 	val n = System.`in`.bufferedReader().readLine().toInt()
-	val dp = IntArray(n + 1)
-	if(n > 1) dp[2] = 3
+	val dp = IntArray(31)
+	dp[2] = 3
 	for (i in 4..n step 2) {
 		dp[i] = dp[i - 2] * 3 + 2
 		for (j in 2 until i - 2 step 2) {
